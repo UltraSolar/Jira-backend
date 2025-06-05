5cccf8509606a3a36d6737c8a92b750d8c506537
Ajay Gupta <ajay.lalchand91999@gmail.com>
2025-06-05 11:34:15 +0530

    dotenv WIP

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from "express" ;
+import dotenv from "dotenv";
 
 const PORT = 9000;
+dotenv.config();
 const app = express();
 
 app.listen(Number(PORT), () => console.log('Server Started'));
 
 app.get('/' , (req , res) => {
 res.send('Home page')
-});
+}); 
